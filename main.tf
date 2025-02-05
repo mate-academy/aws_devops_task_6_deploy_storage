714c23dec17a194eddd2aff6d2e82a77181ef717
Bohdan Mateiko <bohdan.id27182@outlook.com>
2025-02-05 18:34:11 +0200

    adds solution

--- main.tf
+++ main.tf
@@ -1,3 +1,50 @@
 # 1. Deploy an S3 storage bucket 
 
+resource "aws_s3_bucket" "grafana_backups" {
+  bucket = "kjbnvskjbkjb"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
 # 2. Confugure bucket policy to allow grafana iam role to use storage 
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.grafana_backups.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [var.grafana_iam_role_arn]
+    }
+
+    actions = [
+      "s3:ListBucket"
+    ]
+
+    resources = [
+      aws_s3_bucket.grafana_backups.arn
+    ]
+  }
+
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [var.grafana_iam_role_arn]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.grafana_backups.arn}/*"
+    ]
+  }
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,1 @@
-grafana_iam_role_arn = ""
+grafana_iam_role_arn = "arn:aws:iam::762233728792:role/grafana-role"
